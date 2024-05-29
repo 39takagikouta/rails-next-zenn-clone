c4e7b624057d54617bcc122abc00d89964b436c4
39takagikouta <kouta.takagi0728@gmail.com>
2024-05-29 23:55:49 +0900

    サインイン時の通知バーを実装

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -6,7 +6,7 @@ import * as React from 'react'
 import '@/styles/destyle.css'
 import CurrentUserFetch from '@/components/CurrentUserFetch'
 import Header from '@/components/Header'
-
+import Snackbar from '@/components/Snackbar'
 import createEmotionCache from '@/styles/createEmotionCache'
 import theme from '@/styles/theme'
 
@@ -27,6 +27,7 @@ export default function MyApp(props: MyAppProps): JSX.Element {
         <CurrentUserFetch />
         <Header></Header>
         <Component {...pageProps} />
+        <Snackbar></Snackbar>
       </ThemeProvider>
     </CacheProvider>
   )
